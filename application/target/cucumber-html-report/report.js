231178a7e0b4eef72aa1c3f6dd9863cc6201ce3e
Sergio Conti <seracon@gmail.com>
2021-06-02 11:59:14 -0300

    Issue #05: Alta Operario

--- application/target/cucumber-html-report/report.js
+++ application/target/cucumber-html-report/report.js
@@ -82,6 +82,7 @@ formatter.before({
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 <<<<<<< develop
 <<<<<<< Updated upstream
@@ -107,6 +108,13 @@ formatter.before({
 >>>>>>> Se agrega codigo al existente
 =======
 >>>>>>> package-tecnico
+=======
+  "duration": 2707008128,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 137954809,
+>>>>>>> Stashed changes
 =======
   "duration": 2707008128,
   "status": "passed"
@@ -166,6 +174,7 @@ formatter.result({
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 <<<<<<< develop
 <<<<<<< Updated upstream
@@ -188,12 +197,16 @@ formatter.result({
 =======
   "duration": 262149835,
 >>>>>>> Stashed changes
+=======
+  "duration": 262149835,
+>>>>>>> Stashed changes
   "status": "passed"
 });
 formatter.match({
   "location": "SimpleObjectMenuGlue.create_a_simple_object()"
 });
 formatter.result({
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
@@ -219,6 +232,9 @@ formatter.result({
 =======
   "duration": 1453755,
 >>>>>>> Stashed changes
+=======
+  "duration": 1453755,
+>>>>>>> Stashed changes
   "status": "passed"
 });
 formatter.match({
@@ -231,6 +247,7 @@ formatter.match({
   "location": "SimpleObjectMenuGlue.there_are_N_simple_objects(int)"
 });
 formatter.result({
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
@@ -259,6 +276,13 @@ formatter.after({
 >>>>>>> Se agrega codigo al existente
 =======
 >>>>>>> package-tecnico
+=======
+  "duration": 5837807,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 5157159,
+>>>>>>> Stashed changes
 =======
   "duration": 5837807,
   "status": "passed"

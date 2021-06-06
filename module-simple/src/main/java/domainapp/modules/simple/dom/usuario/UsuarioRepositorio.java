8b22cc831399b68913b9c6a71b5403355bd1faa7
JorgeMGodoy <jmgodoy2013@gmail.com>
2021-06-06 19:10:49 -0300

    Issue #17: Se agregan variables al metodo "Usuario create"

--- module-simple/src/main/java/domainapp/modules/simple/dom/usuario/UsuarioRepositorio.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/usuario/UsuarioRepositorio.java
@@ -16,54 +16,46 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package domainapp.modules.simple.dom.impl;
+package domainapp.modules.simple.dom.usuario;
 
-import java.util.List;
-
-import org.datanucleus.query.typesafe.TypesafeQuery;
-
-import org.apache.isis.applib.annotation.Action;
-import org.apache.isis.applib.annotation.ActionLayout;
-import org.apache.isis.applib.annotation.BookmarkPolicy;
-import org.apache.isis.applib.annotation.DomainService;
-import org.apache.isis.applib.annotation.DomainServiceLayout;
-import org.apache.isis.applib.annotation.MemberOrder;
-import org.apache.isis.applib.annotation.NatureOfService;
-import org.apache.isis.applib.annotation.ParameterLayout;
-import org.apache.isis.applib.annotation.Programmatic;
-import org.apache.isis.applib.annotation.SemanticsOf;
+import domainapp.modules.simple.dom.impl.QSimpleObject;
+import domainapp.modules.simple.dom.impl.SimpleObject;
+import org.apache.isis.applib.annotation.*;
 import org.apache.isis.applib.services.eventbus.ActionDomainEvent;
 import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
 import org.apache.isis.applib.services.repository.RepositoryService;
+import org.datanucleus.query.typesafe.TypesafeQuery;
+
+import java.util.List;
 
 @DomainService(
         nature = NatureOfService.VIEW_MENU_ONLY,
-        objectType = "simple.SimpleObjectMenu",
+        objectType = "simple.UsuarioMenu",
         repositoryFor = SimpleObject.class
 )
 @DomainServiceLayout(
         named = "Simple Objects",
         menuOrder = "10"
 )
-public class SimpleObjects {
+public class UsuarioRepositorio {
 
     @Action(semantics = SemanticsOf.SAFE)
     @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT)
     @MemberOrder(sequence = "1")
-    public List<SimpleObject> listAll() {
-        return repositoryService.allInstances(SimpleObject.class);
+    public List<Usuario> listAll() {
+        return repositoryService.allInstances(Usuario.class);
     }
 
 
     @Action(semantics = SemanticsOf.SAFE)
     @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT)
     @MemberOrder(sequence = "2")
-    public List<SimpleObject> findByName(
+    public List<Usuario> findByName(
             @ParameterLayout(named="Name")
             final String name
     ) {
-        TypesafeQuery<SimpleObject> q = isisJdoSupport.newTypesafeQuery(SimpleObject.class);
-        final QSimpleObject cand = QSimpleObject.candidate();
+        TypesafeQuery<Usuario> q = isisJdoSupport.newTypesafeQuery(Usuario.class);
+        final QUsuario cand = QUsuario.candidate();
         q = q.filter(
                 cand.name.indexOf(q.stringParameter("name")).ne(-1)
         );
@@ -72,32 +64,33 @@ public class SimpleObjects {
     }
 
     @Programmatic
-    public SimpleObject findByNameExact(final String name) {
-        TypesafeQuery<SimpleObject> q = isisJdoSupport.newTypesafeQuery(SimpleObject.class);
-        final QSimpleObject cand = QSimpleObject.candidate();
+    public Usuario findByNameExact(final String name) {
+        TypesafeQuery<Usuario> q = isisJdoSupport.newTypesafeQuery(Usuario.class);
+        final QUsuario cand = QUsuario.candidate();
         q = q.filter(
                 cand.name.eq(q.stringParameter("name"))
         );
-        return q.setParameter("name", name)
+        return q.setParameter("name", nombre)
                 .executeUnique();
     }
 
     @Programmatic
     public void ping() {
-        TypesafeQuery<SimpleObject> q = isisJdoSupport.newTypesafeQuery(SimpleObject.class);
-        final QSimpleObject candidate = QSimpleObject.candidate();
+        TypesafeQuery<Usuario> q = isisJdoSupport.newTypesafeQuery(Usuario.class);
+        final QUsuario candidate = QUsuario.candidate();
         q.range(0,2);
-        q.orderBy(candidate.name.asc());
+        q.orderBy(candidate.nombre.asc());
         q.executeList();
     }
 
-    public static class CreateDomainEvent extends ActionDomainEvent<SimpleObjects> {}
+    public static class CreateDomainEvent extends ActionDomainEvent<UsuarioRepositorio> {}
     @Action(domainEvent = CreateDomainEvent.class)
     @MemberOrder(sequence = "3")
-    public SimpleObject create(
-            @ParameterLayout(named="Name")
-            final String name) {
-        return repositoryService.persist(new SimpleObject(name));
+    public Usuario create(
+            @ParameterLayout(named="Dni") final String dni,
+            @ParameterLayout(named="Apellido") final String apellido,
+            @ParameterLayout(named="Nombre") final String nombre) {
+        return repositoryService.persist(new Usuario(dni, apellido, nombre));
     }
 
     @javax.inject.Inject

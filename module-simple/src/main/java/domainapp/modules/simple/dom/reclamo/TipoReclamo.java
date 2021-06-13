4e8e6820ea7a9b99ba2fa0efaecafe8ce5947ca3
JorgeMGodoy <jmgodoy2013@gmail.com>
2021-06-13 18:11:40 -0300

    Issue #1: Se crean enumeradores TipoReclamo y Estado
    Se crean clases relacionadas al alta de Reclamo

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/reclamo/TipoReclamo.java
@@ -0,0 +1,25 @@
+package domainapp.modules.simple.dom.reclamo;
+
+public enum TipoReclamo {
+    Falta_de_agua,
+    Conexion_de_agua,
+    Perdida_de_agua_vereda,
+    Baja_presión,
+    Cerrar_llave_de_vereda,
+    Corte_de_agua,
+    Purga_de_cañieria,
+    Conexión_clandestina,
+    Cloaca_rebalsando,
+    Cloaca_tapada,
+    Olor_cloacal,
+    Conexion_de_cloaca,
+    Otras_Labores,
+    Reparacion_de_vereda,
+    Reparacion_de_asfalto,
+    Cambiar_llave_de_vereda,
+    Perdida_de_agua_C_tierra,
+    Perdida_de_agua_C_Pavim,
+    Mantenimiento_colectora,
+    Limpieza_camara,
+    Muestreo_cloro,
+}

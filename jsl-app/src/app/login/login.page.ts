343453f23a252f6133fabb2c3b2dfe0c7c339724
JorgeMGodoy <jmgodoy2013@gmail.com>
2021-11-24 22:05:37 -0300

    Issue #29: Generar APK para el Sistema
    Se define el paquete login y los servicios Login y Toast

--- jsl-app/src/app/login/login.page.ts
+++ jsl-app/src/app/login/login.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginService } from '././../servicios/login.service';
+import { ToastController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  private loginForm: FormGroup;
+  URLServidorInicial : string = environment.urlServidor;
+  usuario = '';
+  password = '';
+
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute,
+    private router: Router, private formBuilder: FormBuilder, private loginService: LoginService,
+    public toastController: ToastController) { 
+
+      this.loginForm = this.formBuilder.group({
+        usuario: ['', Validators.required],
+        password: ['', Validators.required],
+      });
+    }
 
   ngOnInit() {
+    window.localStorage.URLservidor = this.URLServidorInicial;
   }
+
+  submit() {
+    this.usuario = this.loginForm.controls.usuario.value,
+    this.password = this.loginForm.controls.password.value
+    console.log('entro ??');
+    this.loginService.realizaLogin(this.usuario, this.password)
+      .subscribe(
+        (response) => {
+
+          if (response && response.length) {
+            //Guarda el nombre de usuario en cookie
+            window.localStorage.usuario = this.usuario;
+
+            //Guarda la autenticacion en cookie
+            window.localStorage.autenticacion = btoa(this.usuario + ":" + this.password);
+
+            this.router.navigate(['menu/home'])
+            console.log('entro');
+          }
+        },
+        (error) => {
+          console.log(error);
+          console.log('Respuesta de la API recibida con error: ' + error.statusText);
+          this.loginErroneoToast();
+        })
+  }
+
+  async loginErroneoToast() {
+    const toast = await this.toastController.create({
+      message: 'Usuario o Contraseña incorrecto, vuelva a ingresarlos',
+      duration: 2500
+    });
+    toast.present();
+  }
+
 
 }
--- jsl-app/src/app/servicios/login.service.ts
+++ jsl-app/src/app/servicios/login.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  ConversionBase64: string;
+
+  constructor(private http: HttpClient) { }
+
+  url = environment.urlServidor
+
+  realizaLogin(usuario:String, contrasena:String){
+  this.ConversionBase64 = btoa(usuario+":"+contrasena);
+
+      let headers: HttpHeaders = new HttpHeaders();
+      headers = headers.append('Accept', 'application/json;profile=urn:org.apache.isis/v1');
+      headers = headers.append('Authorization', 'Basic '+this.ConversionBase64);
+
+    const URL = this.url+'/restful/services/simple.SimpleReclamoMenu/actions/listAll/invoke';
+
+    return this.http.get<any>(URL, {headers: headers})  
+
+  } 
+  
 }

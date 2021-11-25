d7532d4c6b8feb04842ef6449ee007c9de6dd054
JorgeMGodoy <jmgodoy2013@gmail.com>
2021-11-24 21:42:30 -0300

    Issue #29: Generar APK para el Sistema
    Se define la home.page

--- jsl-app/src/app/home/home.page.ts
+++ jsl-app/src/app/home/home.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController, ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  constructor() { }
+  constructor(public alertController: AlertController, public navCtrl: NavController, public toastController: ToastController) {}
 
-  ngOnInit() {
+  async alertaLogOut() {
+    const alert = await this.alertController.create({
+      header: 'Cerrar Sesión',
+      message: '¿Esta seguro que desea cerrar la sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+          }
+        }, {
+          text: 'Salir',
+          handler: () => {
+            this.navCtrl.navigateRoot('/login');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
+  
 
 }

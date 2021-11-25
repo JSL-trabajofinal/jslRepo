377f253d06bdc0d0288056bf444126c719567789
JorgeMGodoy <jmgodoy2013@gmail.com>
2021-11-24 21:51:47 -0300

    Issue #29: Generar APK para el Sistema
    Se define todo el paquete menu con sus atributos

--- jsl-app/src/app/menu/menu-routing.module.ts
+++ jsl-app/src/app/menu/menu-routing.module.ts
@@ -6,8 +6,36 @@ import { MenuPage } from './menu.page';
 const routes: Routes = [
   {
     path: '',
-    component: MenuPage
-  }
+    component: MenuPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('../home/home.module').then(
+            (m) => m.HomePageModule),
+      },
+      {
+        path: 'reclamos',
+        loadChildren: () =>
+          import('../reclamos/reclamos.module').then(
+            (m) => m.ReclamosPageModule
+          ),
+      },
+      {
+        path: 'cuadrillas',
+        loadChildren: () =>
+          import('../cuadrillas/cuadrillas.module').then(
+            (m) => m.CuadrillasPageModule
+          ),
+      },
+
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
+  },
 ];
 
 @NgModule({
--- jsl-app/src/app/menu/menu.page.ts
+++ jsl-app/src/app/menu/menu.page.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
 })
-export class MenuPage implements OnInit {
+export class MenuPage {
+  constructor(
+    private menu: MenuController,
+    private navCtrl: NavController,
+    private storage: Storage
+  ) {}
 
-  constructor() { }
+  closeMenu() {
+    this.menu.close();
+  }
 
-  ngOnInit() {
+  logout() {
+    this.storage.set('isUserLoggedIn', false);
+    this.navCtrl.navigateRoot('/login');
+  }
+
+  goToReclamos() {
+    this.navCtrl.navigateRoot('menu/reclamos');
+    this.menu.close();
+  }
+
+  goToCuadrillas() {
+    this.navCtrl.navigateRoot('menu/cuadrillas');
+    this.menu.close();
+  }
+
+  goToHome() {
+    this.navCtrl.navigateRoot('menu/home');
+    this.menu.close();
   }
 
 }

34b2fd49f35de5a5804e44f73dac2d21e9e61b8c
JorgeMGodoy <jmgodoy2013@gmail.com>
2021-11-26 02:37:23 -0300

    Issue #29: Generar APK para el Sistema
    Se crea reclamo-detalle, para mostrar el detalle del reclamo activo

--- jsl-app/src/app/reclamo-detalle/reclamo-detalle.page.ts
+++ jsl-app/src/app/reclamo-detalle/reclamo-detalle.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-reclamo-detalle',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reclamo-detalle.page.scss'],
 })
 export class ReclamoDetallePage implements OnInit {
+  id_Reclamo;
+  datosReclamo;
+  datosCuadrilla;
+  param: any;
+  url = environment.urlServidor;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.param = this.activatedRoute.snapshot.params;
+    if (Object.keys(this.param).length) {
+      this.detallarReclamo(this.param.id_Reclamo);
+    }
   }
+  detallarReclamo(id_Reclamo) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Accept: 'application/json;profile=urn:org.apache.isis/v1',
+        Authorization: 'Basic YWRtaW46cGFzcw==',
+      }),
+    };
+    const URL1 = this.url+'/restful/objects/simple.Reclamo/' + id_Reclamo;
+    this.http.get(URL1, httpOptions).subscribe((resultados) => {
+      this.datosReclamo = resultados;
+    });
 
+    const URL2 = this.url+'/restful/objects/simple.Cuadrilla/' + id_Reclamo;
+    this.http.get(URL2, httpOptions).subscribe((resultados2) => {
+      this.datosCuadrilla = resultados2;
+    });
+
+  }
 }

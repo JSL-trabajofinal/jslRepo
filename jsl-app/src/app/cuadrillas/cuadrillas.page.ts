e76d79ef95d9d13ba044b315991453582cb38ae1
JorgeMGodoy <jmgodoy2013@gmail.com>
2021-11-27 04:53:35 -0300

    Issue #29: Generar APK para el Sistema
    Se crea y se define  el paquete de cuadrilla para listar las cuadrillas

--- jsl-app/src/app/cuadrillas/cuadrillas.page.ts
+++ jsl-app/src/app/cuadrillas/cuadrillas.page.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-cuadrillas',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cuadrillas.page.scss'],
 })
 export class CuadrillasPage implements OnInit {
+  datosCuadrilla;
+  id_Cuadrilla;
+  
+  constructor(private http: HttpClient, private router:Router) { }
 
-  constructor() { }
+  public contenidoArray: any = null;
+  public contenidoArrayTecnico: any = null;
+  url = environment.urlServidor;
 
   ngOnInit() {
+    this.listarCuadrillas();
+  }
+
+  listarCuadrillas(){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Accept: 'application/json;profile=urn:org.apache.isis/v1',
+        Authorization: 'Basic YWRtaW46cGFzcw==',
+      }),
+    };
+    const URL = this.url + '/restful/services/simple.SimpleCuadrillaMenu/actions/listAll/invoke';
+    this.http.get(URL, httpOptions).subscribe((resultados: Array<any>) =>  {
+      var array = resultados;
+      array.pop();
+      this.contenidoArray = array;
+    });
+  }
+
+  obtieneCuadrilla(idCuadrilla){
+    console.log(idCuadrilla);
+    this.router.navigate(['/cuadrilla-detalle', { id_Cuadrilla: idCuadrilla}]);
   }
 
 }

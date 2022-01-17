b23d6af47bb342e9018580f436352ab7bb3a3a3d
Jorge Godoy <jmgodoy2013@gmail.com>
2022-01-16 23:48:14 -0300

    Issue #29: Generar APK para el sistema
    Se corrige page cuadrilla-detalle TS y HTML

--- jsl-app/src/app/cuadrilla-detalle/cuadrilla-detalle.page.ts
+++ jsl-app/src/app/cuadrilla-detalle/cuadrilla-detalle.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-cuadrilla-detalle',
@@ -9,19 +11,23 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./cuadrilla-detalle.page.scss'],
 })
 export class CuadrillaDetallePage{
-  id_Cuadrilla;
+  id;
+  idPlanilla;
   datosReclamo;
   cuadrillas: any;
   param: any;
+  resultadosArraytemp: any;
   url = environment.urlServidor;
   
   constructor(
     private http: HttpClient,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {}
   
   
   public contenidoArray: any = null;
+  public resultadosArrayFiltrado = [];
   
   ngOnInit() {
     this.param = this.activatedRoute.snapshot.params;
@@ -29,25 +35,46 @@ export class CuadrillaDetallePage{
       this.detallarCuadrilla(this.param.id_Cuadrilla);
     }
   }
-  detallarCuadrilla(id_Cuadrilla) {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        Accept: 'application/json;profile=urn:org.apache.isis/v1',
-        Authorization: 'Basic YWRtaW46cGFzcw==',
-      }),
-    };
-    const URL = this.url+'/restful/objects/simple.Cuadrilla/' + id_Cuadrilla + '/collections/reclamosAsignados';
-    this.http.get(URL, httpOptions).subscribe((cuadrillas: Array<any>) =>  {
-      var array = cuadrillas;
-      array.pop();
-      this.contenidoArray = array;
-    });
-  
+
+
+    detallarCuadrilla(id_Cuadrilla) {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          Accept: 'application/json;profile=urn:org.apache.isis/v1',
+          Authorization: 'Basic YWRtaW46cGFzcw==',
+        }),
+      };
+      const URL = this.url+'/restful/objects/simple.Cuadrilla/' + id_Cuadrilla + '/collections/reclamosAsignados';
+      this.http.get(URL, httpOptions).subscribe((cuadrillas: Array<any>) =>  {
+        var array = cuadrillas;
+        array.pop();
+        this.contenidoArray = array;
+        this.resultadosArraytemp = this.contenidoArray;
     
+        const largoArray = this.resultadosArraytemp.length;
+  
+        for (var i = 0; i < largoArray; ) {
+          if (this.resultadosArraytemp[i].hasOwnProperty('estado')) {
+            if (this.resultadosArraytemp[i].estado != 'Cerrado' && this.resultadosArraytemp[i].estado != 'Anulado') {
+              this.resultadosArrayFiltrado.push(this.contenidoArray[i]);
+            }
+          }
+          i = i + 1;
+        }
+        this.resultadosArrayFiltrado.sort(((a, b) => a.nroReclamo - b.nroReclamo));
+        this.contenidoArray = this.resultadosArrayFiltrado;
+      });
     }
-  
-  
+
+
+    cargarPlanilla(id) {
+      console.log("ingresando a cargarPlanilla " + id);
+      this.router.navigate(['/crear-planilla', { idReclamo: id }]);
+    }
+
+    obtenerPlanilla(idPlanilla) {
+      console.log("ingresando a obtenerPlanilla " + idPlanilla);
+      this.router.navigate(['/planilla', { id: idPlanilla }]);
+    }
    
   }
-  
-  

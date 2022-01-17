1829f94977339797d407ca988a16621b362799a2
Jorge Godoy <jmgodoy2013@gmail.com>
2022-01-17 00:41:11 -0300

    Issue #29: Generar APK para el sistema
    Se modifica metodo alertaLogOut de home.page

--- jsl-app/src/app/home/home.page.ts
+++ jsl-app/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { AlertController, NavController, ToastController } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -9,30 +10,34 @@ import { AlertController, NavController, ToastController } from '@ionic/angular'
 })
 export class HomePage {
 
-  constructor(public alertController: AlertController, public navCtrl: NavController, public toastController: ToastController) {}
+  constructor(public router: Router) {}
 
-  async alertaLogOut() {
-    const alert = await this.alertController.create({
-      header: 'Cerrar Sesión',
-      message: '¿Esta seguro que desea cerrar la sesión?',
-      buttons: [
-        {
-          text: 'Cancelar',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: (blah) => {
-          }
-        }, {
-          text: 'Salir',
-          handler: () => {
-            this.navCtrl.navigateRoot('/login');
-          }
-        }
-      ]
-    });
-
-    await alert.present();
-  }
+  alertaLogOut(){
+    Swal.fire({
+      title: "Esta seguro que desea cerrar la sesión",
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: "Si",
+      confirmButtonColor: "#68DE65",
+      cancelButtonText: "No",
+      cancelButtonColor: "#DF4343",
+  })
+  .then(resultado => {
+      if (resultado.isConfirmed) {
+        Swal.fire({
+          title: "Sesión cerrada exitosamente!",
+                icon: 'success', 
+                confirmButtonColor: "#68DE65",               
+              })
+          this.router.navigate(['/login']);
+      } else {
+        Swal.fire({
+          title: "Acción cancelada ", 
+          confirmButtonColor: "#68DE65",
+          icon: 'error',
+                 })
+      }
+  });
+    }
   
-
-}
+  }
